404cd9f21e651e9a9eaf5e94caf420af66394483
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-08 20:49:36 +0100

    triggering event callbacks

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import { User } from "./models/User";
 const user = new User({ name: "Michal", age: 27 });
 
 user.on("change", () => {
-  console.log("change");
+  console.log("change #1");
 });
 user.on("change", () => {
-  console.log("change");
+  console.log("change #2");
+});
+user.on("save", () => {
+  console.log("Save was triggered");
 });
 
-console.log(user);
+user.trigger("change");
+user.trigger("save");
