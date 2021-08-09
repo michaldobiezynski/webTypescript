a2bb061905dd5960c9e68770ff103864a787bb57
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-09 08:23:07 +0100

    successfully fetching model data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import axios from "axios";
+import { User } from "./models/User";
 
-axios.post("http://localhost:3000/users", {
-  name: "mynmae",
-  age: 20,
-});
+const user = new User({ id: 4 });
+
+user.fetch();
