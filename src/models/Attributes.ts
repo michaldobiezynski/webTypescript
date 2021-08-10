77a021ab96ac4b182f54e6db68e6fe4eefed3b12
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-10 08:54:12 +0100

    An Advanced Generic Constraint

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,8 +1,8 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get<K extends keyof T>(propName: K): T[K] {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
