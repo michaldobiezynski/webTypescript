62c27dd7361e7a957d042cf049996a5fa9ba4223
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-10 08:27:55 +0100

    extracting an attributes class

--- src/models/User.ts
+++ src/models/User.ts
@@ -15,13 +15,5 @@ export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps) {}
-
-  get<K extends keyof UserProps>(propName: K): UserProps[K] {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  
 }
