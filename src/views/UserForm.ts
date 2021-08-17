2896f32a76fe190f491a884c72b343c873ee7777
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-17 08:28:51 +0100

    Reading Input Text

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -15,9 +15,17 @@ export class UserForm {
     return {
       // "click:button": this.onButtonClick,
       "click:.set-age": this.onSetAgeClick,
+      "click:.set-name": this.onSetNameClick,
     };
   }
 
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("input");
+
+    const name = input.value;
+
+    this.model.set({ name });
+  };
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
   };
@@ -32,7 +40,7 @@ export class UserForm {
         <div>User name: ${this.model.get("name")}</div>
         <div>User age: ${this.model.get("age")}</div>
         <input />
-        <button>Click Me</button>
+        <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
       </div>
     `;
