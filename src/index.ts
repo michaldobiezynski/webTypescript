af7f94f4744de7b1f96976edbe0adb470bc96d65
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-18 21:19:55 +0100

    Testing Region Mapping

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { User } from "./models/User";
-import { UserForm } from "./views/UserForm";
+import { UserEdit } from "./views/UserEdit";
 
 const user = User.buildUser({ name: "Bob", age: 20 });
 
 const root = document.getElementById("root");
 if (root) {
-  const userform = new UserForm(root, user);
-  userform.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error("Root element not found.");
 }
--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -2,6 +2,12 @@ import { UserProps, User } from "./../models/User";
 import { View } from "./View";
 
 export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: ".user-show",
+      userForm: ".user-form",
+    };
+  }
   template(): string {
     return `   
     <div>
