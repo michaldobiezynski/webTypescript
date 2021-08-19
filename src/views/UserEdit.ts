8b3ffe8f6d0486c58cdb6f025202360cb99d4903
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-08-19 08:20:49 +0100

    View Nesting

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -1,3 +1,5 @@
+import { UserForm } from "./UserForm";
+import { UserShow } from "./UserShow";
 import { UserProps, User } from "./../models/User";
 import { View } from "./View";
 
@@ -8,6 +10,16 @@ export class UserEdit extends View<User, UserProps> {
       userForm: ".user-form",
     };
   }
+
+  onRender(): void {
+    // do our nesting!
+    const userShow = new UserShow(this.regions.userShow, this.model);
+    userShow.render();
+
+    const userForm = new UserForm(this.regions.userForm, this.model);
+    userForm.render();
+  }
+
   template(): string {
     return `   
     <div>
